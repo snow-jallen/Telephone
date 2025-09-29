4bc095d23782339a40c3fc3bceb24d7514623dc4
unknown <aidan.anderson@ENGINEERING.SNOW>
2025-09-29 15:15:16 -0600

    add aidanapi (the funky one)

--- AidanApi/Program.cs
+++ AidanApi/Program.cs
@@ -1,3 +1,5 @@
+using System.Net.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
@@ -7,6 +9,13 @@ builder.AddServiceDefaults();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
+builder.Services.AddHttpClient("kadenapi", client =>
+{
+    client.BaseAddress = new Uri("https+http://kadenapi");
+});
+
 
 var app = builder.Build();
 
@@ -23,5 +32,27 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+bool retry = true;
+
+app.MapGet("/Call", async (string message, IHttpClientFactory factory) => {
+
+    var client = factory.CreateClient("kadenapi");
+
+    if (retry)
+    {
+        var response = await client.GetAsync($"/Call?message={message}");
+        retry = false;
+        return await response.Content.ReadAsStringAsync();
+    }
+    else
+    {
+        return message + ", ends in aidanAPI";
+    }
+});
+
+
+
 
 app.Run();
