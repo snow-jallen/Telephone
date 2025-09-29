67898a3f6363c1400142105ca824a42e9b8df3d7
jonathan.allen <jonathan.allen@ENGR4F8P5S3>
2025-09-29 15:15:38 -0600

    let aiden call kaden

--- Telephone.AppHost/AppHost.cs
+++ Telephone.AppHost/AppHost.cs
@@ -1,11 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var aidan = builder.AddProject<Projects.AidanApi>("aidanapi");
+var aidan = builder.AddProject<Projects.AidanApi>("aidanapi");    
 
 var omner = builder.AddProject<Projects.OmnerApi>("omnerapi")
     .WithReference(aidan);
 
 var kaden = builder.AddProject<Projects.KadenApi>("kadenapi")
     .WithReference(omner);
+
+aidan.WithReference(kaden);
 
 builder.Build().Run();
